2986b23cb61e1a4ff4eb06347f1b2a7a7041a855
Z3DDIEZ <zawadinyachiya4@gmail.com>
2026-03-19 18:31:16 +0200

    feat: reInitialize Firebase Functions project with `package.json` and ESLint configuration.

---
+++ functions/eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  {
+    ignores: ["lib", "node_modules"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  }
+);
